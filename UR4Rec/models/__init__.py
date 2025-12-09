bc7fe4080ac50fb23b7ae45886515f098f341098
ww93 <54789158+ww93@users.noreply.github.com>
2025-12-09 14:56:37 +0800

    Add unified UR4Rec module and MoE retriever block

--- UR4Rec/models/__init__.py
+++ UR4Rec/models/__init__.py
@@ -4,6 +4,8 @@ UR4Rec Models Package.
 from .user_preference_retriever import UserPreferenceRetriever
 from .llm_reranker import LLMReranker, LLMInterface, OpenAILLM, AnthropicLLM, LocalLLM
 from .ur4rec import UR4Rec
+from .ur4rec_unified import UR4RecUnified, UR4RecLossOutput
+from .retriever_moe import RetrieverMoEBlock, CrossModalExpert
 
 __all__ = [
     'UserPreferenceRetriever',
@@ -12,5 +14,9 @@ __all__ = [
     'OpenAILLM',
     'AnthropicLLM',
     'LocalLLM',
-    'UR4Rec'
+    'UR4Rec',
+    'UR4RecUnified',
+    'UR4RecLossOutput',
+    'RetrieverMoEBlock',
+    'CrossModalExpert',
 ]
